5d2fc7d1fda2a8224a385fabc901704148b122b0
kgkrishnaa <kgkrishnaa@gmail.com>
2022-12-01 10:13:26 +0000

    End of Sprint 1

--- features/environment.py
+++ features/environment.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 
 def before_all(context):
-    context.browser = webdriver.Chrome(r"C:\Users\Admin\LBG-Python-API\webdrivers\chromedriver.exe")
+    context.browser = webdriver.Chrome(r"C:\Users\Admin\final\LBG7-Python-API\webdrivers\chromedriver.exe")
     context.browser.maximize_window()
 
 def after_all(context):
